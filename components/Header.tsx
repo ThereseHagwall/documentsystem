0c180dea42549b56a1da6ca4ac3be7883415adba
Therese Hagwall <theresejager@live.se>
2023-10-18 11:05:57 +0200

    Initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+  return (
+    <header className="sticky top-0 flex items-center justify-center h-24 bg-purple-400 text-black ">
+      <h1 className="text-4xl  ">Dokumenthanteringssystem</h1>
+    </header>
+  )
+}
