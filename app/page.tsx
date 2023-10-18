3cbaf382439301ba22b00e743d519b53e0280e0a
Therese Hagwall <theresejager@live.se>
2023-10-18 11:43:36 +0200

    Add a navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-
+    <div className='flex flex-col p-10'>
       <div>😒</div>
+    </div>
 
 
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 flex items-center justify-center h-24 bg-purple-400 text-black ">
-      <h1 className="text-4xl  ">Dokumenthanteringssystem</h1>
+    <header className="sticky top-0 flex items-center justify-around h-24 bg-purple-400 text-black ">
+      <Link href="/"><h1 className="text-4xl  ">Dokumenthanteringssystem</h1></Link>
+      <nav>
+        <ul className="list-none flex gap-2">
+          <li><Link href="/">Startsida</Link></li>
+          <li><Link href="/documentList">Dokument</Link></li>
+        </ul>
+      </nav>
     </header>
   )
 }
