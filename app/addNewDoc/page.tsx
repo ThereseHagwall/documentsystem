206ee9efd48ea4e150b8538d9015f4cd13ff21c9
Therese Hagwall <theresejager@live.se>
2023-10-26 23:15:57 +0200

    small fix

--- app/addNewDoc/page.tsx
+++ app/addNewDoc/page.tsx
@@ -15,6 +15,7 @@ export default function Page() {
         author: "",
         content: "",
     });
+    const [errorMessage, setErrorMessage] = useState<string>("");
 
     const apiKey = process.env.NEXT_PUBLIC_API_KEY;
     const router = useRouter();
@@ -38,6 +39,13 @@ export default function Page() {
 
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
+
+        if (formData.content.trim() === "") {
+            setErrorMessage("Alla fälten måste fyllas i innan du skickar.");
+            return;
+        } else {
+            setErrorMessage("");
+        }
 
         try {
             const response = await fetch("/api", {
@@ -69,69 +77,78 @@ export default function Page() {
                 Lägg till nytt dokument
             </h1>
             <div className="max-w-xl mt-7 m-auto p-4 bg-slate-300 rounded ">
-                <label
-                    htmlFor="title"
-                    className="text-lg text-black font-semibold"
-                >
-                    Titel:
-                </label>
-                <input
-                    className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500 shadow-2xl"
-                    type="text"
-                    id="title"
-                    name="title"
-                    value={formData.title}
-                    onChange={handleInputChange}
-                    required
-                />
-                <label
-                    htmlFor="author"
-                    className="text-lg font-semibold text-black"
-                >
-                    Skapad av:
-                </label>
-                <input
-                    className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500 shadow-2xl"
-                    type="text"
-                    id="author"
-                    name="author"
-                    value={formData.author}
-                    onChange={handleInputChange}
-                    required
-                />
-                <label
-                    className="text-lg text-black font-semibold"
-                    htmlFor="editor"
-                >
-                    Innehåll:
-                </label>
-                <div className="shadow-2xl">
-                    <Editor
-                        id="editor"
-                        apiKey={apiKey}
-                        init={{
-                            plugins:
-                                "ai mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss",
-                            toolbar:
-                                "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
-                            ai_request: (request: Request, respondWith: any) =>
-                                respondWith.string(() =>
-                                    Promise.reject(
-                                        "See docs to implement AI Assistant"
-                                    )
-                                ),
-                        }}
-                        value={formData.content}
-                        onEditorChange={handleEditorChange}
+                <form onSubmit={handleSubmit} action="">
+                    <label
+                        htmlFor="title"
+                        className="text-lg text-black font-semibold"
+                    >
+                        Titel:
+                    </label>
+                    <input
+                        className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500 shadow-2xl"
+                        type="text"
+                        id="title"
+                        name="title"
+                        value={formData.title}
+                        onChange={handleInputChange}
+                        required
                     />
-                </div>
-                <button
-                    className="bg-blue-500 mt-2 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 transition-all cursor-pointer"
-                    type="submit"
-                    onClick={handleSubmit}
-                >
-                    Skicka
-                </button>
+                    <label
+                        htmlFor="author"
+                        className="text-lg font-semibold text-black"
+                    >
+                        Skapad av:
+                    </label>
+                    <input
+                        className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500 shadow-2xl"
+                        type="text"
+                        id="author"
+                        name="author"
+                        value={formData.author}
+                        onChange={handleInputChange}
+                        required
+                    />
+                    <label
+                        className="text-lg text-black font-semibold"
+                        htmlFor="editor"
+                    >
+                        Innehåll:
+                    </label>
+                    <div className="shadow-2xl">
+                        <Editor
+                            id="editor"
+                            apiKey={apiKey}
+                            init={{
+                                plugins:
+                                    "ai mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss",
+                                toolbar:
+                                    "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+                                ai_request: (
+                                    request: Request,
+                                    respondWith: any
+                                ) =>
+                                    respondWith.string(() =>
+                                        Promise.reject(
+                                            "See docs to implement AI Assistant"
+                                        )
+                                    ),
+                            }}
+                            value={formData.content}
+                            onEditorChange={handleEditorChange}
+                        />
+                    </div>
+                    <button
+                        className="bg-blue-500 mt-2 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 transition-all cursor-pointer "
+                        type="submit"
+                    >
+                        Skicka
+                    </button>
+                    {errorMessage && (
+                        <p className="text-red-500 text-sm mt-2">
+                            {errorMessage}
+                        </p>
+                    )}
+                </form>
             </div>
         </div>
     );
